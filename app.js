81095919a3124cd8d4d05701e85510facaa3fe09
Jossue Proano <jossuepatric@hotmail.com>
2026-02-09 17:28:17 -0500

    fix: update CORS configuration to allow requests from localhost:4200

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+// const express = require('express');
+// const cors = require('cors');
+// const morgan = require('morgan');
+// const app = express();
+
+// // settings
+// app.set('puerto', process.env.PORT || 3000);
+// app.set('nombreApp', 'Gestión de empleados');
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(morgan('dev'));
+// app.use('/api/empleados', require('./routes/empleados.routes'));
+
+// module.exports = app;
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -7,7 +22,7 @@ const app = express();
 app.set('puerto', process.env.PORT || 3000);
 app.set('nombreApp', 'Gestión de empleados');
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:4200' }));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api/empleados', require('./routes/empleados.routes'));
